46e75648e31460e943cb28f020eff6ef65a8be8e
Juan I. Casais <juanignaciocasais@gmail.com>
2023-10-30 22:35:55 -0300

    Ajuste nombre Personaje

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function llamarApi() {
     document.getElementById("llamar-api").setAttribute("disabled", "");
 
     fetch('https://api.gameofthronesquotes.xyz/v1/random')
-        //fetch('https://api.gameofthronesquotes.xyz/v1/character/Ygritte')
         .then(response => {
             if (response.ok) {
                 return response.json(); // Parse the response data as JSON 
@@ -20,15 +19,14 @@ function llamarApi() {
         })
         .then(data => {
             // Process the response data here 
-            document.getElementById("nombre").innerText = data.character.name.split(" ", 1);
-            console.log(data.character.house.name);
+            let name = data.character.slug;
+            document.getElementById("nombre").innerText = name.charAt(0).toUpperCase() + name.slice(1);;
+
             if (data.character.house.name == null) {
                 casaPersonaje = "sin casa";
-
             } else {
                 casaPersonaje = data.character.house.name;
             }
-            console.log(data);
         })
         .catch(error => {
             // Handle any errors here 
